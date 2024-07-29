d27118c7cda283d50036d3ea9d21f28e7310a925
Ruslan Simakov <UA667766706@GMAIL.COM>
2024-07-29 20:19:51 +0100

    Test commit 1

---
+++ src/main/java/ruslan/simakov/only_test/OnlyTestApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.only_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruslan/simakov/only_test/OnlyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.only_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
